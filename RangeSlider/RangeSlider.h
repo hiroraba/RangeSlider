5dc7b467f46d38080074cb2cfde804eb6ced18c7
hiroraba <xornelius0313@gmail.com>
2015-12-23 23:31:07 +0900

    Initial Commit

---
+++ RangeSlider/RangeSlider.h
@@ -0,0 +1,19 @@
+//
+//  RangeSlider.h
+//  RangeSlider
+//
+//  Created by matsuohiroki on 2015/12/23.
+//  Copyright © 2015年 matsuohiroki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RangeSlider.
+FOUNDATION_EXPORT double RangeSliderVersionNumber;
+
+//! Project version string for RangeSlider.
+FOUNDATION_EXPORT const unsigned char RangeSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RangeSlider/PublicHeader.h>
+
+
